4c795fafea958db9acd9eba9fca980b3393a3836
juanda <juandacorreo@gmail.com>
2017-03-31 14:26:11 +0200

    driver mysql change

--- diccionario2.py
+++ diccionario2.py
@@ -22,7 +22,7 @@ def encode(word, encoding):
     > pip install chardet
     '''
     tmp = word.decode(encoding)  # unicode
-    return tmp
+    return tmp.encode('utf-8')
     # tmp.encode('utf-8')
 
 
@@ -69,16 +69,16 @@ class MyPrettyPrinter(pprint.PrettyPrinter):
         return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
 
 
-idiomas = [{"id_idioma": 1,"idioma_ru": "Russian","idioma_abrev": "ru"},{"id_idioma": 2,"idioma_en": "Romanian","idioma_abrev": "ro"}, {"id_idioma": 3,"idioma_en": "Arabic","idioma_abrev": "ar"}, {"id_idioma": 4,"idioma_en": "Chinese","idioma_abrev": "zh"}, {"id_idioma": 5,"idioma_en": "Bulgarian","idioma_abrev": "bg"}, {"id_idioma": 6,"idioma_en": "Polish","idioma_abrev": "pl"}, {"id_idioma": 7,"idioma_en": "English","idioma_abrev": "en"}, {"id_idioma": 8,"idioma_en": "French","idioma_abrev": "fr"}, {"id_idioma": 9,"idioma_en": "Catalan","idioma_abrev": "ca"}, {"id_idioma": 10,"idioma_en": "Euskera","idioma_abrev": "eu"}, {"id_idioma": 11,"idioma_en": "German","idioma_abrev": "de"}, {"id_idioma": 12,"idioma_en": "Italian","idioma_abrev": "it"}, {"id_idioma": 13,"idioma_en": "Portuguese","idioma_abrev": "pt"}, {"id_idioma": 14,"idioma_en": "Galician","idioma_abrev": "ga"}, {"id_idioma": 15,"idioma_en": "Brazilian Portuguese","idioma_abrev": "br"}, {"id_idioma": 16,"idioma_en": "Croatian","idioma_abrev": "cr"}, {"id_idioma": 17,"idioma_en": "Valencian","idioma_abrev": "val"}]
+idiomas = [{"id_idioma": 1,"idioma_ru": "Russian","idioma_abrev": "ru"},{"id_idioma": 2,"idioma_en": "Romanian","idioma_abrev": "ro"}, {"id_idioma": 4,"idioma_en": "Chinese","idioma_abrev": "zh"}, {"id_idioma": 5,"idioma_en": "Bulgarian","idioma_abrev": "bg"}, {"id_idioma": 6,"idioma_en": "Polish","idioma_abrev": "pl"}, {"id_idioma": 7,"idioma_en": "English","idioma_abrev": "en"}, {"id_idioma": 8,"idioma_en": "French","idioma_abrev": "fr"}, {"id_idioma": 9,"idioma_en": "Catalan","idioma_abrev": "ca"}, {"id_idioma": 10,"idioma_en": "Euskera","idioma_abrev": "eu"}, {"id_idioma": 11,"idioma_en": "German","idioma_abrev": "de"}, {"id_idioma": 12,"idioma_en": "Italian","idioma_abrev": "it"}, {"id_idioma": 13,"idioma_en": "Portuguese","idioma_abrev": "pt"}, {"id_idioma": 14,"idioma_en": "Galician","idioma_abrev": "ga"}, {"id_idioma": 15,"idioma_en": "Brazilian Portuguese","idioma_abrev": "br"}, {"id_idioma": 16,"idioma_en": "Croatian","idioma_abrev": "cr"}, {"id_idioma": 17,"idioma_en": "Valencian","idioma_abrev": "val"}]
 
 analisis = ""
 
 palabras = obtenerPalabras()
 
-for palabra in palabras:
-    analisis = analisis + " " + palabra['word']
-encoding = chardet.detect(analisis).get('encoding')
-print " Encoding: " + encoding
+#for palabra in palabras:
+#    analisis = analisis + " " + palabra['word']
+#encoding = chardet.detect(analisis).get('encoding')
+#print " Encoding: " + encoding
 
 #for palabra in palabras:
 #    #palabra['word'] = encode(palabra['word'], encoding)
@@ -91,12 +91,19 @@ for idioma in idiomas:
     print idioma
     table = "traducciones_" + str(idioma['id_idioma'])
     palabras = obtenerTraducciones(table)
+    analisis=""
     for palabra in palabras:
-       analisis = analisis + " " + palabra['word']
+        if palabra['def'] is None:
+            palabra['def']=""
+        if palabra['word'] is None:
+            palabra['word']=""
+        analisis = analisis + " " + palabra['def'] + " " + palabra['word']
     encoding = chardet.detect(analisis).get('encoding')
-    print " Encoding: " + encoding
+    print " Encoding expected to be: " + encoding
     for palabra in palabras:
-        palabra['word'] = encode(palabra['word'], encoding)
+        palabra['word'] = encode(palabra['word'], 'ISO-8859-1')
+        palabra['def'] = encode(palabra['def'], 'ISO-8859-1')
+        #print palabra['def']
     fileName = "words_" + idioma['idioma_abrev'] + ".txt"
     with codecs.open(fileName, 'w', encoding='utf-8') as outfile:
-        json.dump(palabras, outfile, indent=4, sort_keys=True, default = myconverter, ensure_ascii=False)
+        json.dump(palabras, outfile, indent=4, sort_keys=True, default = myconverter, ensure_ascii=False, encoding='utf8')
